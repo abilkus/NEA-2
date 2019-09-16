54f5d457b1f30619e6553dc7d7f1d09860f70a92
abilkus <48215890+abilkus@users.noreply.github.com>
2019-09-16 14:28:47 +0100

    Update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 # Create your views here.
-
+from django.http import HttpResponse
 from .models import Music, Composer, MusicInstance, Genre
 
 
